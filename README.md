7413420e01cbdc9d086114ca37071876e2bdedef
Ariana Berger <ariana.berger@gmail.com>
2018-02-14 22:38:00 +0000

    updated cli

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is the README.. tbd
+
+##NOTES
+1. loops in CLI.rb not working
--- lib/cli.rb
+++ lib/cli.rb
@@ -30,6 +30,7 @@ class CommandLineInterface
     if input == "1"
       #execute NYC method for list of restaurants 1-5
       puts "Let's check out the restaurants in NYC..."
+      list_nyc
         #get info from restaurant list for NYC
           #pull into on an individual restaurants
     elsif input == "2"
@@ -38,11 +39,23 @@ class CommandLineInterface
       puts "Let's check out the restaurants in DC"
     elsif input == "4"
       puts "Let's check out the restaurants in LA"
-    else
+    else #this needs to loop properly!
       puts "I'm sorry, please enter the number or city name you'd like to dine in tonight:"
       input = gets.strip
     end
   end
 
+  def list_nyc
+    puts "Here are the NYC restaurants"
+    puts " 1. 2. 3. 4"
+    puts "Which would one would you like to know more about?"
+    input = gets.strip
+    if input == "1"
+      puts "GREAT"
+    else #this needs to loop properly!
+      puts "Which would one would you like to know more about?"
+      input = gets.strip
+    end
+  end
 
 end
